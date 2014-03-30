f10596b10b11245ebf361dc1edf2896b180271de
Burton Williams <burton.b.williams@gmail.com>
2014-03-30 19:12:09 -0400

    initial commit of hallie site

---
+++ js/myplaylist.js
@@ -0,0 +1,127 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/just_fine.m4a',
+        title:'Just Fine',
+        artist:'Mary J Blige',
+        rating:4,
+        duration:'4:02',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/workin_day_and_night.m4a',
+        oga:'mix/1.ogg',
+        title:'Work day and night',
+        artist:'Michael Jackson',
+        rating:4,
+        duration:'5:13',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    }
+];
